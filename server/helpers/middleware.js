57b454a1e2af53c6cb7c9c073ea8e99e8400d200
Igor <qgosha@list.ru>
2018-10-23 20:01:09 -0600

    deploy

--- server/helpers/middleware.js
+++ server/helpers/middleware.js
@@ -2,7 +2,7 @@ const loggedIn = (req, res, next) => {
     if (req.user) {
         next();
     } else {
-        res.status(401).send('you are not logged in').redirect('/login');
+        res.redirect('/login');
     }
 }
 
