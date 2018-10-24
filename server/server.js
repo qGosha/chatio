f81afc1143a0710189722322606b5b32ffe3edf4
Igor <qgosha@list.ru>
2018-10-23 21:59:06 -0600

    deploy

--- server/server.js
+++ server/server.js
@@ -37,6 +37,15 @@ require('./routes/signUpRoutes')(app);
 require('./routes/basicRoutes')(app);
 require('./routes/profileRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 
