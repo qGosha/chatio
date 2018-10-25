3e0309e9df857d7ee13546a447636226db5e640c
Igor <qgosha@list.ru>
2018-10-24 19:53:44 -0600

    deploy

--- server/server.js
+++ server/server.js
@@ -38,9 +38,9 @@ require('./routes/basicRoutes')(app);
 require('./routes/profileRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'));
-
   const path = require('path');
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
