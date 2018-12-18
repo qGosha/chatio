133b2f78414ac73a737fb093579e2b5370497253
Igor <qgosha@list.ru>
2018-12-17 21:37:05 -0700

    s

--- server/client/src/components/Messages.js
+++ server/client/src/components/Messages.js
@@ -3,19 +3,33 @@ import { Segment, Image } from 'semantic-ui-react'
 
 const standartImage = 'https://react.semantic-ui.com/images/wireframe/square-image.png';
 
-
+const styles = {
+  messageContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: '10px',
+    alignItems: 'center'
+  },
+  avatar: {
+    height: '40px',
+    weight: '40px'
+  },
+  text: {
+    wordBreak: 'break',
+    marginLeft: '8px'
+  }
+}
 
 const Messages = ({messages, friendOptions}) => {
   const message = messages && messages.map( item => {
     return(
-      <div>
-       <Image src=>
-      <div>
+      <div key={item._id} style={styles.messageContainer}>
+       <Image src={standartImage} style={styles.avatar}/>
+       <span style={styles.text}>{item.message && item.message.text}</span>
+      </div>
     )
   })
- return(
-
- )
+ return message
 }
 
 export default Messages;
