e6e27e0bbd42cbb6e15bc6aa75762ad73400a9f1
Igor <qgosha@list.ru>
2019-05-06 20:38:53 -0600

    change_socket_endpoint

--- server/client/src/containers/Dashboard.js
+++ server/client/src/containers/Dashboard.js
@@ -210,8 +210,8 @@ class Dashboard extends Component {
   componentDidMount() {
     this.audio.load()
     this.normalTitle = document.title
-    // const socket = io("https://im-messenger.herokuapp.com");
-    const socket = io("localhost:5000")
+    const socket = io("https://im-messenger.herokuapp.com");
+    // const socket = io("localhost:5000")
     this.props.setSocket(socket)
     const keys = {
       hidden: "visibilitychange",
